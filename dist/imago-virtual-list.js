e164f4f07e463c12d6a7b6a1f043b0256856d0c1
Sebastian Moreno <sebastian@nex9.com>
2015-10-30 17:01:44 -0400

    small improvement virtual list

--- dist/imago-virtual-list.js
+++ dist/imago-virtual-list.js
@@ -21,6 +21,7 @@ ImagoVirtualList = (function() {
         });
         self = {};
         self.scrollTop = 0;
+        self.browserKey = (typeof bowser !== "undefined" && bowser !== null ? bowser.safari : void 0) <= 8 ? '-webkit-transform' : 'transform';
         if (!scope.imagovirtuallist.offsetBottom) {
           scope.imagovirtuallist.offsetBottom = $window.innerHeight;
         }
@@ -65,7 +66,7 @@ ImagoVirtualList = (function() {
           return self.updateDisplayList();
         };
         self.updateDisplayList = function() {
-          var browserKey, cellsToCreate, chunks, data, findIndex, firstCell, i, idx, l;
+          var cellsToCreate, chunks, data, findIndex, firstCell, i, idx, l;
           firstCell = Math.max(Math.round(self.scrollTop / masterDiv.clientHeight) - (Math.round($window.innerHeight / masterDiv.clientHeight)), 0);
           cellsToCreate = Math.min(firstCell + self.numberOfCells, self.numberOfCells);
           data = firstCell * self.itemsPerRow;
@@ -73,7 +74,6 @@ ImagoVirtualList = (function() {
           chunks = _.chunk(scope.visibleProvider, self.itemsPerRow);
           i = 0;
           l = scope.visibleProvider.length;
-          browserKey = (typeof bowser !== "undefined" && bowser !== null ? bowser.safari : void 0) <= 8 ? '-webkit-transform' : 'transform';
           while (i < l) {
             findIndex = function() {
               var chunk, idx, indexChunk, indexItem, item, j, k, len, len1;
@@ -94,7 +94,7 @@ ImagoVirtualList = (function() {
             };
             idx = findIndex();
             scope.visibleProvider[i].styles = {};
-            scope.visibleProvider[i].styles[browserKey] = "translate(" + ((masterDiv.clientWidth * idx.inside) + 'px') + ", " + ((firstCell + idx.chunk) * masterDiv.clientHeight + 'px') + ")";
+            scope.visibleProvider[i].styles[self.browserKey] = "translate(" + ((masterDiv.clientWidth * idx.inside) + 'px') + ", " + ((firstCell + idx.chunk) * masterDiv.clientHeight + 'px') + ")";
             i++;
           }
           if (scope.imagovirtuallist.scroll) {
