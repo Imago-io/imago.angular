6d3053eaa946a62587c73b1db608d45a5b9c2bd9
Sebastian Moreno <sebastian@nex9.com>
2016-03-23 10:15:16 -0400

    putting back checkbox

--- dist/imago-fields.js
+++ dist/imago-fields.js
@@ -1,4 +1,47 @@
 (function() {
+  var ImagoFieldCheckbox;
+
+  ImagoFieldCheckbox = (function() {
+    function ImagoFieldCheckbox() {
+      return {
+        replace: true,
+        require: 'ngModel',
+        scope: {
+          ngModel: '='
+        },
+        transclude: true,
+        templateUrl: '/imago/imago-field-checkbox.html',
+        link: function(scope, element, attrs, ngModelController) {
+          if (attrs.disabled) {
+            scope.disabled = true;
+          }
+          attrs.$observe('disabled', function(value) {
+            return scope.disabled = value;
+          });
+          return scope.update = function(value) {
+            if (scope.disabled) {
+              return;
+            }
+            value = !value;
+            ngModelController.$setViewValue(value);
+            ngModelController.$render();
+            if (attrs.required) {
+              return ngModelController.$setValidity('required', value);
+            }
+          };
+        }
+      };
+    }
+
+    return ImagoFieldCheckbox;
+
+  })();
+
+  angular.module('imago').directive('imagoFieldCheckbox', [ImagoFieldCheckbox]);
+
+}).call(this);
+
+(function() {
   var ImagoFieldCurrency, ImagoFieldCurrencyController, imagoFilterCurrency;
 
   ImagoFieldCurrency = (function() {
@@ -237,6 +280,7 @@
 
 }).call(this);
 
-angular.module("imago").run(["$templateCache", function($templateCache) {$templateCache.put("/imago/imago-field-currency.html","<div class=\"imago-field-content-currency imago-field currency\"><div ng-class=\"{expanded: fieldcurrency.expanded}\"><button ng-click=\"fieldcurrency.expanded = !fieldcurrency.expanded\" class=\"ii ii-caret-left\"></button><div ng-class=\"{expanded: fieldcurrency.expanded}\" class=\"fields\"><div ng-if=\"!fieldcurrency.expanded\" ng-class=\"{focus:onfocus}\" class=\"wrapper compact\"><label><span ng-repeat=\"cur in fieldcurrency.currencies\" ng-click=\"fieldcurrency.currency = cur\" ng-class=\"{active: fieldcurrency.currency === cur, invalid: fieldcurrency.notComplete[cur]}\">{{cur}}</span></label><input type=\"text\" imago-filter-currency=\"imago-filter-currency\" ng-model=\"fieldcurrency.ngModel[fieldcurrency.currency]\" ng-model-options=\"{updateOn: \'blur\'}\" ng-change=\"update(fieldcurrency.ngModel); onfocus = false\" ng-disabled=\"!fieldcurrency.currency\" ng-focus=\"onfocus = true\"/></div><div ng-repeat=\"cur in fieldcurrency.currencies\" ng-if=\"fieldcurrency.expanded\" ng-class=\"{invalid: fieldcurrency.notComplete[cur]}\" class=\"wrapper expanded\"><div class=\"imago-field\"><label>{{cur}}</label><input type=\"text\" currency=\"{{cur}}\" imago-filter-currency=\"imago-filter-currency\" ng-model=\"fieldcurrency.ngModel[cur]\" ng-model-options=\"{updateOn: \'blur\'}\" ng-blur=\"update(fieldcurrency.ngModel)\"/></div></div></div></div></div>");
+angular.module("imago").run(["$templateCache", function($templateCache) {$templateCache.put("/imago/imago-field-checkbox.html","<div class=\"imago-checkbox\"><label ng-class=\"{active: ngModel, disabled: disabled}\" ng-click=\"update(ngModel)\" class=\"topcoat-checkbox\"><div class=\"topcoat-checkbox__checkmark\"></div><span ng-transclude=\"ng-transclude\"></span></label></div>");
+$templateCache.put("/imago/imago-field-currency.html","<div class=\"imago-field-content-currency imago-field currency\"><div ng-class=\"{expanded: fieldcurrency.expanded}\"><button ng-click=\"fieldcurrency.expanded = !fieldcurrency.expanded\" class=\"ii ii-caret-left\"></button><div ng-class=\"{expanded: fieldcurrency.expanded}\" class=\"fields\"><div ng-if=\"!fieldcurrency.expanded\" ng-class=\"{focus:onfocus}\" class=\"wrapper compact\"><label><span ng-repeat=\"cur in fieldcurrency.currencies\" ng-click=\"fieldcurrency.currency = cur\" ng-class=\"{active: fieldcurrency.currency === cur, invalid: fieldcurrency.notComplete[cur]}\">{{cur}}</span></label><input type=\"text\" imago-filter-currency=\"imago-filter-currency\" ng-model=\"fieldcurrency.ngModel[fieldcurrency.currency]\" ng-model-options=\"{updateOn: \'blur\'}\" ng-change=\"update(fieldcurrency.ngModel); onfocus = false\" ng-disabled=\"!fieldcurrency.currency\" ng-focus=\"onfocus = true\"/></div><div ng-repeat=\"cur in fieldcurrency.currencies\" ng-if=\"fieldcurrency.expanded\" ng-class=\"{invalid: fieldcurrency.notComplete[cur]}\" class=\"wrapper expanded\"><div class=\"imago-field\"><label>{{cur}}</label><input type=\"text\" currency=\"{{cur}}\" imago-filter-currency=\"imago-filter-currency\" ng-model=\"fieldcurrency.ngModel[cur]\" ng-model-options=\"{updateOn: \'blur\'}\" ng-blur=\"update(fieldcurrency.ngModel)\"/></div></div></div></div></div>");
 $templateCache.put("/imago/imago-field-date.html","<div class=\"imago-field-date-content imago-field date\"><div ng-transclude=\"ng-transclude\"></div><input type=\"text\" date-time=\"date-time\" dismiss=\"true\" ng-model=\"ngModel\" ng-blur=\"update(ngModel)\" view=\"date\" min-view=\"date\" partial=\"true\"/></div>");
 $templateCache.put("/imago/imago-field-number.html","<div class=\"imago-field-number-content imago-field number\"><div ng-transclude=\"ng-transclude\"></div><input type=\"text\" ng-model=\"ngModel\" ng-model-options=\"{\'updateOn\': \'blur\'}\" ng-change=\"update(ngModel)\" ng-disabled=\"disabled\"/><button type=\"button\" ng-disabled=\"isOverMin() || disabled\" ng-click=\"decrement()\" class=\"decrement\"></button><button type=\"button\" ng-disabled=\"isOverMax() || disabled\" ng-click=\"increment()\" class=\"increment\"></button></div>");}]);
---
+++ src/imago-fields/imago-field-checkbox.directive.coffee
@@ -0,0 +1,28 @@
+class ImagoFieldCheckbox extends Directive
+
+  constructor: ->
+
+    return {
+
+      replace: true
+      require: 'ngModel'
+      scope:
+        ngModel: '='
+      transclude: true
+      templateUrl: '/imago/imago-field-checkbox.html'
+
+      link: (scope, element, attrs, ngModelController) ->
+
+        scope.disabled = true if attrs.disabled
+
+        attrs.$observe 'disabled', (value) ->
+          scope.disabled = value
+
+        scope.update = (value) ->
+          return if scope.disabled
+          value = !value
+          ngModelController.$setViewValue(value)
+          ngModelController.$render()
+          ngModelController.$setValidity('required', value) if attrs.required
+
+    }
